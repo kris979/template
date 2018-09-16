25daea4eb2dfeca41e837cd74d91d15e37744811
kris <kris979@gmail.com>
2018-09-16 17:48:38 +0100

    starting up

--- src/main/java/tree/BinaryTree.java
+++ src/main/java/tree/BinaryTree.java
@@ -1,9 +1,27 @@
 package tree;
 
 public class BinaryTree {
+	
+	private Node root;
 
-	public BinaryTree() {
-		// TODO Auto-generated constructor stub
+	public BinaryTree(Node root) {
+		super();
+		this.root = root;
+	}
+
+	public final Node getRoot() {
+		return root;
+	}
+
+	@Override
+	public String toString() {
+		return "BinaryTree [root=" + root + "]";
+	}
+
+	public static void main(String[] args) {
+		Node root = new Node(null, null, 0);
+		BinaryTree tree = new BinaryTree(root);
+		System.out.println(tree);
 	}
 
 }
--- src/main/java/tree/Node.java
+++ src/main/java/tree/Node.java
@@ -2,8 +2,26 @@ package tree;
 
 public class Node {
 
+	private final Node left, right;
+	private final Integer value;
 	
-	public Node() {
+	public Node(Node left, Node right, Integer value) {
+		super();
+		this.left = left;
+		this.right = right;
+		this.value = value;
+	}
+
+	public final Node getLeft() {
+		return left;
+	}
+
+	public final Node getRight() {
+		return right;
+	}
+
+	public final Integer getValue() {
+		return value;
 	}
 
 }
