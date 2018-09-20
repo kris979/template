b762369167621158523aace22bb41705c492a62f
kris <kris979@gmail.com>
2018-09-20 19:58:35 +0100

    adding placeholder for coderbyte challanges

---
+++ src/test/java/com/coderbyte/CoderbyteTest.java
@@ -0,0 +1,14 @@
+package com.coderbyte;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CoderbyteTest {
+
+	@Test
+	public void test() {
+//		fail("Not yet implemented");
+	}
+
+}
