a6f3ae8b5567b8ef2343841a62f18f4eb063b90b
kris <kris979@gmail.com>
2018-09-21 17:24:08 +0100

    adding placeholder for questions from google

---
+++ src/test/java/google/GoogleTest.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package google;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author kris9
+ *
+ */
+public class GoogleTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
