4776cbf576598bf79bb068c37e76461761085257
kris <kris979@gmail.com>
2018-09-21 11:15:01 +0100

    reversing string

--- src/test/java/coderbyte/CoderbyteTest.java
+++ src/test/java/coderbyte/CoderbyteTest.java
@@ -1,14 +1,45 @@
 package coderbyte;
 
-import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.junit.Test;
 
 public class CoderbyteTest {
 
+	public static String longestWord(String sen) {
+		Pattern p = Pattern.compile("[a-zA-Z]+");
+		Matcher m = p.matcher(sen);
+		String longestWord = "";
+		while (m.find()) {
+			if (m.group().length() > longestWord.length()) {
+				longestWord = m.group();
+			}
+		}
+		return longestWord;
+	}
+	
+	public static String reverse(String input) {
+		char[] charArray = input.toCharArray();
+		int start = 0;
+		int end = charArray.length-1;
+		while (start < end) {
+			char tmp = charArray[start];
+			charArray[start] = charArray[end];
+			charArray[end] = tmp;
+			start +=1;
+			end-=1;
+		}
+		return new String(charArray);
+	}
+
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		assertThat(longestWord("fun&!! time"), is("time"));
+		assertThat(reverse("Coderbyte"), is("etybredoC"));
 	}
 
 }
