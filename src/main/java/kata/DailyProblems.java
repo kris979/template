5b74a92a305466206dd95d6a6f5a4cf26715df7b
kris <kris979@gmail.com>
2018-09-22 22:37:21 +0100

    Daily Coding Problem: Problem #4

--- src/main/java/kata/DailyProblems.java
+++ src/main/java/kata/DailyProblems.java
@@ -28,19 +28,19 @@ public class DailyProblems {
 	 * @return
 	 */
 	public char firstNonRepeatingChar(String input) {
-		
+
 		LinkedHashMap<Character, Integer> counterMap = new LinkedHashMap<>();
-		
+
 		char[] charArray = input.toCharArray();
 		for (char c : charArray) {
 			Integer counter = counterMap.get(c);
 			if (counter != null) {
 				counterMap.put(c, ++counter);
 			} else {
-				counterMap.put(c,1);
+				counterMap.put(c, 1);
 			}
 		}
-		
+
 		Set<Entry<Character, Integer>> counters = counterMap.entrySet();
 		for (Entry<Character, Integer> entry : counters) {
 			Integer value = entry.getValue();
@@ -48,7 +48,7 @@ public class DailyProblems {
 				return entry.getKey();
 			}
 		}
-		
+
 		return 0;
 	}
 
@@ -59,19 +59,19 @@ public class DailyProblems {
 	 */
 	public int numberOfDistinctPairsThatAddUpToK(int[] input, int k) {
 		Set<Integer> reminders = new HashSet<>();
-		
+
 		int counter = 0;
 		for (Integer integer : input) {
 			int reminder = k - integer;
 			if (reminders.contains(integer)) {
-				counter+=1;
+				counter += 1;
 			} else {
 				reminders.add(reminder);
 			}
 		}
 		return counter;
 	}
-	
+
 	/**
 	 * @param input
 	 */
@@ -82,46 +82,45 @@ public class DailyProblems {
 		int counter = 0;
 		int index = 0;
 		for (int i = 0; i < charArray.length; i++) {
-			Character c = charArray[i]; 
+			Character c = charArray[i];
 			if (c.equals(previous)) {
-				counter+=1;
+				counter += 1;
 			} else {
 				String largestSoFar = substrings.get(previous);
 				if (largestSoFar == null || largestSoFar.length() < counter) {
-					substrings.put(previous, new String(charArray,index,counter));
+					substrings.put(previous, new String(charArray, index, counter));
 					counter = 1;
 					index = i;
 				}
 			}
-			previous=c;
+			previous = c;
 		}
 		String largestSoFar = substrings.get(previous);
 		if (largestSoFar == null || largestSoFar.length() < counter) {
-			substrings.put(previous, new String(charArray,index,counter));
+			substrings.put(previous, new String(charArray, index, counter));
 		}
 		System.out.println(substrings);
 	}
-	
-	
+
 	/**
 	 * number of ways you can climb the stairs
 	 * 
 	 * you can jump by 1,2 or 3 steps at a time
 	 * 
-	 * if jump was only allowed by 1 or 2 steps then we woudl remove else and 
-	 * int output = numWays(input-1) + numWays(input-2);
+	 * if jump was only allowed by 1 or 2 steps then we woudl remove else and int
+	 * output = numWays(input-1) + numWays(input-2);
 	 */
 	public int numWays(int input) {
-		if( input == 0 || input == 1) {
+		if (input == 0 || input == 1) {
 			return 1;
 		} else if (input == 2) {
-			int output = numWays(input-1) + numWays(input-2);
-			return  output;
+			int output = numWays(input - 1) + numWays(input - 2);
+			return output;
 		}
-		int output = numWays(input-1) + numWays(input-2) + numWays(input-3);
+		int output = numWays(input - 1) + numWays(input - 2) + numWays(input - 3);
 		return output;
 	}
-	
+
 	/**
 	 * i.e. aaabbcc becomes a3b2c2
 	 * 
@@ -130,25 +129,25 @@ public class DailyProblems {
 		if (input == null || input.equals("")) {
 			return input;
 		}
-		
+
 		StringBuilder s = new StringBuilder();
 		Character previous = input.toCharArray()[0];
 		int counter = 0;
-		for (Character c  : input.toCharArray()) {
+		for (Character c : input.toCharArray()) {
 			if (c.equals(previous)) {
-				counter+=1;
+				counter += 1;
 			} else {
 				s.append(previous);
 				s.append(counter);
 				counter = 1;
 			}
-			previous=c;
+			previous = c;
 		}
 		s.append(previous);
 		s.append(counter);
 		return s.toString();
 	}
-	
+
 	/**
 	 * @param a
 	 * @param b
@@ -159,46 +158,47 @@ public class DailyProblems {
 			throw new IllegalArgumentException();
 		}
 		int[] result = new int[2];
-		int commonDenominator = leastCommonDenominator(a[1],b[1]);
-		int numeratorMultipA = commonDenominator/a[1];
-		int numeratorMultipB = commonDenominator/b[1];
-		int lA = a[0]*numeratorMultipA;
-		int lB = b[0]*numeratorMultipB;
+		int commonDenominator = leastCommonDenominator(a[1], b[1]);
+		int numeratorMultipA = commonDenominator / a[1];
+		int numeratorMultipB = commonDenominator / b[1];
+		int lA = a[0] * numeratorMultipA;
+		int lB = b[0] * numeratorMultipB;
 		result[0] = lA + lB;
 		result[1] = commonDenominator;
-		
+
 		return result;
 	}
-	
+
 	private int leastCommonDenominator(int a, int b) {
 		List<Integer> multiplesA = new ArrayList<>();
 		List<Integer> multiplesB = new ArrayList<>();
 		multiplesA.add(a);
 		multiplesB.add(b);
 		int index = 0;
-		for(;;) {
+		for (;;) {
 			Integer lcd = multiplesA.get(index);
 			if (multiplesB.contains(lcd)) {
 				return lcd;
 			} else {
-				index+=1;
-				multiplesA.add(index*a);
-				multiplesB.add(index*b);
-				}
+				index += 1;
+				multiplesA.add(index * a);
+				multiplesB.add(index * b);
+			}
 		}
 	}
 
 	/**
-	 * For example for 3 vendors in the shop: [{0, 4, 5}, {2, 8, 3}, {7, 11, 10}], the result should be [{0, 2, 5}, {2, 8, 3}, {8, 11, 10}]”
+	 * For example for 3 vendors in the shop: [{0, 4, 5}, {2, 8, 3}, {7, 11, 10}],
+	 * the result should be [{0, 2, 5}, {2, 8, 3}, {8, 11, 10}]”
 	 */
 	public static int lowestPrice(int[] timeSlot) {
-		
+
 		class Vendor {
-			
+
 			int start;
 			int end;
 			int price;
-			
+
 			public Vendor(int start, int end, int price) {
 				super();
 				this.start = start;
@@ -214,27 +214,59 @@ public class DailyProblems {
 				}
 			}
 		}
-		
+
 		List<Vendor> vendors = new ArrayList<>();
 		vendors.add(new Vendor(0, 4, 5));
 		vendors.add(new Vendor(2, 8, 3));
 		vendors.add(new Vendor(7, 11, 10));
-		
+
 		List<Integer> validPrices = new ArrayList<>();
-		
+
 		for (Vendor vendor : vendors) {
 			if (vendor.isValid(timeSlot)) {
 				validPrices.add(vendor.price);
 			}
 		}
-		
+
 		if (validPrices.isEmpty()) {
 			throw new IllegalArgumentException("invalid slot");
 		}
-		
+
 		Collections.sort(validPrices);
-		
+
 		return validPrices.get(0);
 	}
-	
+
+	/**
+	 * Given an array of integers, find the first missing positive integer in
+	 * linear time and constant space. 
+	 * In other words, find the lowest positive integer that does not exist in the array. 
+	 * The array can contain duplicates and negative numbers as well. 
+	 * For example, the input [3, 4, -1, 1] should give 2. The input [1, 2, 0] should give 3.
+	 * 
+	 * @param input
+	 * @return
+	 */
+	int findFirstPositiveMissingInteger(int[] input) {
+		int[] present = new int[10];
+
+		for (int i = 0; i < present.length; i++) {
+			present[i] = -1;
+		}
+
+		for (int i : input) {
+			if (i >= 0) {
+				present[i] = 1;
+			}
+		}
+
+		for (int i = 1; i < present.length; i++) {
+			if (present[i] == -1) {
+				return i;
+			}
+		}
+
+		return -1;
+	}
+
 }
--- src/test/java/kata/DailyProblemsTest.java
+++ src/test/java/kata/DailyProblemsTest.java
@@ -51,6 +51,12 @@ public class DailyProblemsTest {
 	
 	@Test
 	public void substringsTest() { 
-		dp.substraings("aaabbaa");
+//		dp.substraings("aaabbaa");
+	}
+	
+	@Test
+	public void findFirstPositiveMissingIntegerTest() {
+		assertThat(dp.findFirstPositiveMissingInteger(new int[] {3, 4, -1, 1}), is(equalTo(2)));
+		assertThat(dp.findFirstPositiveMissingInteger(new int[] {1, 2, 0}), is(equalTo(3)));
 	}
 }
