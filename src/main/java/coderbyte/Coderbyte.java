7464c9e086550a0d351f8496d723e5c02ad610d7
kris <kris979@gmail.com>
2018-10-03 19:42:50 +0100

    commit before branching.

--- src/main/java/coderbyte/Coderbyte.java
+++ src/main/java/coderbyte/Coderbyte.java
@@ -21,35 +21,75 @@ public class Coderbyte {
 		}
 		return longestWord;
 	}
-	
+
 	public String reverse(String input) {
 		char[] charArray = input.toCharArray();
 		int start = 0;
-		int end = charArray.length-1;
+		int end = charArray.length - 1;
 		while (start < end) {
 			char tmp = charArray[start];
 			charArray[start] = charArray[end];
 			charArray[end] = tmp;
-			start +=1;
-			end-=1;
+			start += 1;
+			end -= 1;
 		}
 		return new String(charArray);
 	}
-	
+
 	public int simpleAdding(int num) {
 		if (num == 0) {
 			return 0;
 		}
 		return num + simpleAdding(--num);
 	}
-	
-    public String letterChanges(String str) {
-        StringBuilder result = new StringBuilder();
-        char[] strChars = str.toLowerCase().toCharArray();
-        char[] replacements = {'b', 'c', 'd', 'E', 'f', 'g', 'h', 'I', 'j', 'k', 'l', 'm', 'n', 'O', 'p', 'q', 'r', 's', 't', 'U', 'v', 'w', 'x', 'y', 'z', 'A'};
-        for (int i = 0; i < str.length(); i++) {
-            result.append(strChars[i] >= 'a' && strChars[i] <= 'z' ? replacements[strChars[i] - 97] : strChars[i]);
-        }
-        return result.toString();
-    }
+
+	public String letterChanges(String str) {
+		StringBuilder result = new StringBuilder();
+		char[] strChars = str.toLowerCase().toCharArray();
+		char[] replacements = { 'b', 'c', 'd', 'E', 'f', 'g', 'h', 'I', 'j', 'k', 'l', 'm', 'n', 'O', 'p', 'q', 'r',
+				's', 't', 'U', 'v', 'w', 'x', 'y', 'z', 'A' };
+		for (int i = 0; i < str.length(); i++) {
+			result.append(strChars[i] >= 'a' && strChars[i] <= 'z' ? replacements[strChars[i] - 97] : strChars[i]);
+		}
+		return result.toString();
+	}
+
+	public String capitalize(String input) {
+		StringBuilder sb = new StringBuilder();
+		String[] split = input.split(" ");
+		for (String string : split) {
+			char charAt = string.charAt(0);
+			Character.toUpperCase(charAt);
+			sb.append(Character.toUpperCase(charAt)).append(string.substring(1)).append(" ");
+		}
+		sb.deleteCharAt(sb.length() - 1);
+		return sb.toString();
+	}
+
+
+//	Input:"f++d+"
+	public String simpleSymbols(String input) {
+		char[] array = input.toCharArray();
+		for (int i = 0; i < array.length; i++) {
+			char left = array[i];
+			char middle;
+			if (i+1 >= array.length) {
+				middle = '!';
+			} else {
+				middle = array[i+1];
+			}
+			char right;
+			if (i+2 >= array.length) {
+				right = '!';
+			} else {
+				right = array[i+2];
+			}
+			
+			if (Character.isLetter(middle) && (left != '+' || right != '+')) {
+				return "false";
+			}
+			
+		}
+		return "true";
+	}
 }
--- src/test/java/coderbyte/CoderbyteTest.java
+++ src/test/java/coderbyte/CoderbyteTest.java
@@ -33,4 +33,24 @@ public class CoderbyteTest {
 		assertThat(cb.letterChanges("hello*3"), is("Ifmmp*3"));
 		assertThat(cb.letterChanges("fun times!"), is("gvO Ujnft!"));
 	}
+	
+	@Test
+	public void capitalizeFirstLetterTest() {
+		assertThat(cb.capitalize("hello world"), is("Hello World"));
+		assertThat(cb.capitalize("i ran there"), is("I Ran There"));
+	}
+
+//	TODO:finish it
+//	@Test
+//	public void simpleSymbolsTest() {
+//		assertThat(cb.simpleSymbols("f++d+"), is("false"));
+//		assertThat(cb.simpleSymbols("+d+=3=+s+"), is("true"));
+//		assertThat(cb.simpleSymbols("+f++d+"), is("true"));
+//		assertThat(cb.simpleSymbols("+f++"), is("false"));
+//		assertThat(cb.simpleSymbols("+"), is("false"));
+//		assertThat(cb.simpleSymbols("a"), is("false"));
+////		assertThat(cb.simpleSymbols("+z+z+z+"), is("true"));
+//		assertThat(cb.simpleSymbols("+a++"), is("true"));
+//		assertThat(cb.simpleSymbols("+z+z+==+a+"), is("true"));
+//	}
 }
