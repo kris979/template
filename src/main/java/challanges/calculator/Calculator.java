2331e4072f2b90d6c227359c6f850339b880e70b
kris <kris979@gmail.com>
2020-09-14 16:58:21 +0100

    Adding Calculator - requires improvements.

--- src/main/java/challanges/calculator/Calculator.java
+++ src/main/java/challanges/calculator/Calculator.java
@@ -1,38 +1,129 @@
 package challanges.calculator;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class Calculator {
 
-    public long evaluate(final long leftOperand,final long rightOperand,final Character operator) {
+    private static Set<String> operators = new HashSet<>();
+
+    static {
+        operators.add("+");
+        operators.add("-");
+        operators.add("*");
+        operators.add("/");
+    }
+
+    public long calc(final long leftOperand, final long rightOperand, final String operator) {
         long result = 0;
         switch (operator) {
-            case '+': {
+            case "+": {
                 result = leftOperand + rightOperand;
-            } break;
-            case '-': {
+            }
+            break;
+            case "-": {
                 result = leftOperand - rightOperand;
-            }break;
-            case '*': {
+            }
+            break;
+            case "*": {
                 result = leftOperand * rightOperand;
-            }break;
-            case '/': {
+            }
+            break;
+            case "/": {
                 result = leftOperand / rightOperand;
-            }break;
+            }
+            break;
         }
         return result;
     }
 
-
-    public List<String> convert(final String exp) {
+    public List<String> splitAndTrim(final String exp) {
         String[] chars = exp.split("");
         List<String> result = Arrays.stream(chars)
-                                     .filter(aChar -> !aChar.equalsIgnoreCase(" "))
-                                     .map(aChar -> aChar.trim())
-                                     .collect(
-                                             Collectors.toList());
+                                    .filter(aChar -> !aChar.equalsIgnoreCase(" "))
+                                    .map(aChar -> aChar.trim())
+                                    .collect(
+                                            Collectors.toList());
         return result;
+    }
+
+    public String calc(final String input) {
+
+        final List<String> exp = splitAndTrim(input);
+
+        //rearrange expression with operator on the end i.e. 2 3 + instead of 2 + 3
+        final List<String> postFixExp = rearrange(exp);
+
+        System.out.println(postFixExp);
+
+        //evaluate expression
+        Stack<String> stack = new Stack<>();
+        for (String s : postFixExp) {
+            if (isClosingBracket(s)) {
+                //ignore it
+            } else if (isOperator(s)) {
+                String rightOperand = stack.pop();
+                String leftOperand = stack.pop();
+                if (!stack.empty() && stack.peek()
+                                           .equals("(")) {
+                    stack.pop();
+                }
+                stack.push(Long.toString(calc(Long.parseLong(leftOperand), Long.parseLong(rightOperand), s)));
+            } else {
+                stack.push(s);
+            }
+        }
+        return stack.pop();
+    }
+
+    private boolean isClosingBracket(final String s) {
+        return s.equalsIgnoreCase(")");
+    }
+
+    private boolean isOpeningBracket(final String s) {
+        return s.equalsIgnoreCase("(");
+    }
+
+    public List<String> rearrange(final List<String> expression) {
+
+        LinkedList<String> rearranged = new LinkedList<>();
+
+        String previous = null;
+        String mem = null;
+
+        for (int i = 0; i < expression.size(); i++) {
+            String current = expression.get(i);
+            if (isDigit(current) && isOperator(previous)) {
+                String last = rearranged.removeLast();
+                rearranged.addLast(current);
+                rearranged.addLast(last);
+            } else if (isOpeningBracket(current) && isOperator(previous)) {
+                mem = previous;
+                rearranged.set(i - 1, current);
+            } else if (isClosingBracket(current)) {
+                rearranged.add(current);
+                if (mem != null) {
+                    rearranged.add(mem);
+                    mem = null;
+                }
+            } else {
+                rearranged.add(current);
+            }
+            previous = current;
+        }
+        return rearranged;
+    }
+
+    private boolean isDigit(final String operand) {
+        try {
+            Integer.parseInt(operand);
+        } catch (NumberFormatException e) {
+            return false;
+        }
+        return true;
+    }
+
+    public boolean isOperator(final String input) {
+        return operators.contains(input);
     }
 }
--- src/test/java/challanges/calculator/CalculatorTest.java
+++ src/test/java/challanges/calculator/CalculatorTest.java
@@ -2,11 +2,13 @@ package challanges.calculator;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
 import java.util.List;
 
-import static org.hamcrest.CoreMatchers.hasItem;
-import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.hasItems;
+import static org.hamcrest.Matchers.contains;
 import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
 
 class CalculatorTest {
 
@@ -14,19 +16,44 @@ class CalculatorTest {
 
     @Test
     void evaluate() {
-        assertThat(calculator.evaluate(2, 3, '+'), is(5L));
-        assertThat(calculator.evaluate(10, 3, '-'), is(7L));
-        assertThat(calculator.evaluate(10, 3, '*'), is(30L));
-        assertThat(calculator.evaluate(30, 3, '/'), is(10L));
-//        assertThat(challanges.calculator.evaluate("2+3")), is(5L));
+        assertThat(calculator.calc(2, 3, "+"), is(5L));
+        assertThat(calculator.calc(10, 3, "-"), is(7L));
+        assertThat(calculator.calc(10, 3, "*"), is(30L));
+        assertThat(calculator.calc(30, 3, "/"), is(10L));
+        assertThat(calculator.calc("2+3"), is("5"));
+        assertThat(calculator.calc("(2+3)"), is("5"));
+        assertThat(calculator.calc("(2+3)*4"), is("20"));
+        assertThat(calculator.calc("(2+3)*4-2"), is("18"));
+        assertThat(calculator.calc("(2+3)+(1+5)"), is("11"));
     }
 
     @Test
     void split() {
-        assertThat(calculator.convert("2 + 3"), hasItem("2"));
-        assertThat(calculator.convert("2+3"), hasItem("3"));
-        List<String> split = calculator.convert("2 + 3 ");
-        System.out.println(split);
-        System.out.println(split.size());
+        List<String> convert = calculator.splitAndTrim("2 + 3");
+        assertThat(convert, hasItems("2", "3"));
+        assertThat(convert.size(), is(3));
+        convert = calculator.splitAndTrim("2+3");
+        assertThat(convert, hasItems("2", "3"));
+        assertThat(convert.size(), is(3));
+        convert = calculator.splitAndTrim("2 +3");
+        assertThat(convert, hasItems("2", "3"));
+        assertThat(convert.size(), is(3));
     }
+
+    @Test
+    void rearrange() {
+        List<String> cmd = Arrays.asList("2", "+", "3");
+        assertThat(calculator.rearrange(cmd), contains("2", "3", "+"));
+        List<String> cmd1 = Arrays.asList("2", "-", "3");
+        assertThat(calculator.rearrange(cmd1), contains("2", "3", "-"));
+        List<String> cmd2 = Arrays.asList("(","2", "+", "3",")");
+        assertThat(calculator.rearrange(cmd2), contains("(","2","3","+",")"));
+        List<String> cmd3 = Arrays.asList("(","2", "+", "3",")","-","1");
+        assertThat(calculator.rearrange(cmd3), contains("(","2","3","+",")","1","-"));
+        List<String> cmd4 = Arrays.asList("(","2", "+", "3",")","*","5","+","2");
+        assertThat(calculator.rearrange(cmd4), contains("(","2","3","+",")","5","*","2","+"));
+        List<String> cmd5 = Arrays.asList("(","2", "+", "3",")","+","(","5","*","2",")");
+        assertThat(calculator.rearrange(cmd5), contains("(","2","3","+",")","(","5","2","*",")","+"));
+    }
+
 }
